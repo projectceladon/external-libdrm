dd26eb6b8e8bf700d07e80b22a2eeb6e0c770eab
Kishore Kadiyala <kishore.kadiyala@intel.com>
2017-08-14 14:57:08 +0530

    Add Conditional check for compilation
    Signed-off-by: Kishore Kadiyala <kishore.kadiyala@intel.com>

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@
 # IN THE SOFTWARE.
 #
 
+ifeq ($(LIBDRM_VER),intel)
 LIBDRM_COMMON_MK := $(call my-dir)/Android.common.mk
 
 LOCAL_PATH := $(call my-dir)
@@ -65,3 +66,4 @@ include $(LIBDRM_COMMON_MK)
 include $(BUILD_SHARED_LIBRARY)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
