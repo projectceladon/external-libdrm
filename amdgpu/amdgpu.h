8d8357dc6482c771d2f9501257c29abe8b06ba9c
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2023-02-24 10:01:53 +0100

    amdgpu: add support for querying VM faults information
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>

--- amdgpu/amdgpu.h
+++ amdgpu/amdgpu.h
@@ -1283,6 +1283,22 @@ int amdgpu_query_video_caps_info(amdgpu_device_handle dev, unsigned cap_type,
                                  unsigned size, void *value);
 
 /**
+ * Query information about VM faults
+ *
+ * The return sizeof(struct drm_amdgpu_info_gpuvm_fault)
+ *
+ * \param   dev         - \c [in] Device handle. See #amdgpu_device_initialize()
+ * \param   size        - \c [in] Size of the returned value.
+ * \param   value       - \c [out] Pointer to the return value.
+ *
+ * \return   0 on success\n
+ *          <0 - Negative POSIX Error code
+ *
+*/
+int amdgpu_query_gpuvm_fault_info(amdgpu_device_handle dev, unsigned size,
+				  void *value);
+
+/**
  * Read a set of consecutive memory-mapped registers.
  * Not all registers are allowed to be read by userspace.
  *
